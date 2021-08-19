9ecac983fadb9e7e5c73faed16e78c6b9f46abd6
Arnab Biswas <khanp4397@gmail.com>
2021-08-19 18:43:45 +0530

    fixing type

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -5,7 +5,7 @@ import Cards from './Cards'
 export default function Feedback() {
     return (
         <div>
-            <h1 className="bg-blue-600 text-center font-bold py-3 text-xl rounded">Bank</h1>
+            <h1 className="bg-blue-600 text-center font-bold py-3 text-xl rounded">FEEDBACK</h1>
             <Cards title="Share" text="Tell Your Friends" colour="bg-blue-600" />
         </div>
     )
